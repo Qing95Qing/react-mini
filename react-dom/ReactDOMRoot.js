3fcb1010f676360ef8777bf0bba0a48bba95e84c
Qing95Qing <849832144@qq.com>
2024-12-24 23:27:57 +0800

    add domRoot

--- react-dom/ReactDOMRoot.js
+++ react-dom/ReactDOMRoot.js
@@ -3,4 +3,26 @@
 export function createRoot(container) {
     const root = createContainer(container, ConcurrentRoot);
     markContainerAsRoot(root.current, container);
+    listenToAllSupportedEvents(root);
+    return new ReactDOMRoot(root);
 }
+
+function ReactDOMRoot(internalRoot) {
+  this._internalRoot = internalRoot;
+}
+
+ReactDOMRoot.prototype.render = function(children) {
+    const root = this._internalRoot;
+    updateContainer(children, root);
+};
+
+ReactDOMRoot.prototype.unmount = function() {
+    const root = this._internalRoot;
+    if (root !== null) {
+      this._internalRoot = null;
+      const container = root.containerInfo;
+      updateContainerSync(null, root, null, null);
+      flushSyncWork();
+      unmarkContainerAsRoot(container);
+    }
+};
